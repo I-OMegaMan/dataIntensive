180690198cf61e276ddf38490be6a476ef085300
mmyers <nt3058@gmail.com>
2024-02-06 07:31:41 -0500

    added first script

---
+++ asm1/nodeSetup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt-get install openjdk-8-jdk -y
+mkdir ~/hw1/
+cd ~/hw1/
+wget https://dlcdn.apache.org/hadoop/common/hadoop-3.3.6/hadoop-3.3.6.tar.gz
+tar xvfz hadoop-3.3.6.tar.gz
+~/hw1/hadoop-3.3.6
+#hadoop is going to look for /bin/java so only give it the home directory
+export JAVA_HOME=/usr
+export HADOOP_HOME=~/hadoop-3.3.6
