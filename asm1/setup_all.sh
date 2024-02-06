6776b50957b66b56ead820ecd57ab4249d93005b
mmyers <nt3058@gmail.com>
2024-02-06 14:02:26 -0500

    script now works for two config files

--- asm1/setup_all.sh
+++ asm1/setup_all.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 sshCmd="ssh -i ~/.ssh/cloudprovider"
-input="./cluster.cfg"
-masterSetup="./setup_master.sh"
-echo "beginning"
+workers="./workers.cfg"
+master="./master.cfg"
+configScript="./setup_master.sh"
+
+masterNode=$(head -n1 $master)
+echo "Setting up master $masterNode"
+cat $configScript | $sshCmd $masterNode
+
 while IFS= read -r line
 do
     echo "setup for $line"
-    cat $masterSetup | $sshCmd $line
-done < "$input"
+    cat $configScript | $sshCmd $line
+done < "$workers"
 #~/hw1/hadoop-3.3.6
 #hadoop is going to look for /bin/java so only give it the home directory
 #export JAVA_HOME=/usr
