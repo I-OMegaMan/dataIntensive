66569675939c7120657b0b84d8667e131fb163f0
mmyers <nt3058@gmail.com>
2024-02-13 08:05:04 -0500

    updated to mostly working config scripts

--- asm1/setup_all.sh
+++ asm1/setup_all.sh
@@ -9,6 +9,14 @@ xmlConfigScript="./hadoopConfig/xml_config.sh"
 configScript="./setup_master.sh"
 format_master="./format_master.sh"
 
+#remote directory locations
+#Config files need to be copied to this location
+#Note ~ resolves to THIS SHELL's home not the remote one so don't use it
+remoteHadoopDir=/users/$CLOUDLAB_USER/hadoop/hadoop-3.3.6
+hadoopEtc=$remoteHadoopDir/etc/hadoop/
+#end of remote directory locations
+
+#NAMENODE CONFIG set up the master node aka name node
 masterNode=$(head -n1 $master)
 echo "Setting up master $masterNode"
 masterSsh=$sshCmd$masterNode
@@ -16,10 +24,14 @@ cat $configScript | $masterSsh
 cat $format_master | $masterSsh
 echo "xml config setup for master $master"
 cat $xmlConfigScript | $masterSsh
-scp -i $sshId ./hadoopConfig/*.xml ${CLOUDLAB_USER}@$masterNode:~/hadoop/config/
+#copy the config files to the hadoop etc folder
+scp -i $sshId ./hadoopConfig/*.xml ${CLOUDLAB_USER}@$masterNode:$hadoopEtc
 echo "setting up salves file"
-scp -i $sshId ./workers.cfg ${CLOUDLAB_USER}@$masterNode:~/hadoop/config/slaves
+scp -i $sshId ./workers.cfg ${CLOUDLAB_USER}@$masterNode:$hadoopEtc/workers
+#End of master config
 
+
+#WORKER CONFIG Set up each worker
 while IFS= read -r line
 do
     workerSSH=$sshCmd$line
@@ -27,12 +39,7 @@ do
     cat $configScript | $workerSSH
     echo "xml config setup for ${line}"
     cat $xmlConfigScript | $workerSSH
-    scp -i $sshId ./hadoopConfig/*.xml ${CLOUDLAB_USER}@$line:~/hadoop/config/
-    
+    scp -i $sshId ./hadoopConfig/*.xml ${CLOUDLAB_USER}@$line:$hadoopEtc
 done < "$workers"
-#~/hw1/hadoop-3.3.6
-#hadoop is going to look for /bin/java so only give it the home directory
-#export JAVA_HOME=/usr
-#export HADOOP_HOME=~/hadoop-3.3.6
-
+#end of worker setup
  
