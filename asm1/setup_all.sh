c956db0e70b618b7d3a6e3371fb33f60b921eb47
mmyers <nt3058@gmail.com>
2024-02-22 18:05:46 -0500

    added experiment code

--- asm1/setup_all.sh
+++ asm1/setup_all.sh
@@ -1,13 +1,40 @@
 #!/bin/bash
+#setup_all.sh
+#Used to do the initial setup of hadoop on a newly created
+#cloudlab cluster
+
+#USERNAME CONFIGURATION
+#Make sure you create user.cfg and have it define
+#CLOUDLAB_USER=your user name
+#user.cfg is separate so you don't end up with your username
+#on the public github
 . "./user.cfg" #get the user config and CLOUDLAB_USER
-sshId=~/.ssh/cloudprovider #which SSH identity to use
+
+#SSH CONFIGURATION
+#which SSH identity to use This is the path to the
+#public key you gave cloudlab. If this key is not set up
+#and associated with your cloud lab user ssh will
+#give you error messages.
+sshId=~/.ssh/cloudprovider 
 sshCmd="ssh -i ${sshId} ${CLOUDLAB_USER}@"
-workers="./workers.cfg" #worker host names or IP addresses separated by net lines
-master="./master.cfg"  #master host name or IP address (one only)
+scpCmd="scp -i ${sshId} ${CLOUDLAB_USER}@"
+
+#WORKER AND MASTER CONFIG
+#Create workers.cfg and master.cfg
+#in the directory of this script
+#Again, this is done because you don't want these addresses
+#to appear in the public git repo
+#worker host names or PUBLIC IP addresses separated by new lines
+workers="./workers.cfg"
+#master host name or PUBLIC IP address (one only)
+master="./master.cfg"
 
 xmlConfigScript="./hadoopConfig/xml_config.sh"
+#This script is used to
+#1. Download and install Java
+#2. Download and install Hadoop
+#3. Clone this git repo onto the machine
 configScript="./setup_master.sh"
-format_master="./format_master.sh"
 
 #remote directory locations
 #Config files need to be copied to this location
@@ -21,13 +48,12 @@ masterNode=$(head -n1 $master)
 echo "Setting up master $masterNode"
 masterSsh=$sshCmd$masterNode
 cat $configScript | $masterSsh
-cat $format_master | $masterSsh
-echo "xml config setup for master $master"
-cat $xmlConfigScript | $masterSsh
+#cat $format_master | $masterSsh
+
 #copy the config files to the hadoop etc folder
-scp -i $sshId ./hadoopConfig/*.xml ${CLOUDLAB_USER}@$masterNode:$hadoopEtc
-echo "setting up salves file"
-scp -i $sshId ./workers.cfg ${CLOUDLAB_USER}@$masterNode:$hadoopEtc/workers
+#scp -i $sshId ./hadoopConfig/*.xml ${CLOUDLAB_USER}@$masterNode:$hadoopEtc
+#echo "setting up salves file"
+#scp -i $sshId ./workers.cfg ${CLOUDLAB_USER}@$masterNode:$hadoopEtc/workers
 #End of master config
 
 
