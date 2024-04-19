505b58e6f6ee49778593e4a813bc268ad4ea7b3a
mmyers <nt3058@gmail.com>
2024-04-19 11:53:07 -0400

    fixed issue where master and slave setup operated in the wrong folder

--- project/remote_setup_slave.sh
+++ project/remote_setup_slave.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ~
 git clone https://github.com/I-OMegaMan/dataIntensive.git
-cd ./dataIntensive/spark
+cd ./dataIntensive/project
 chmod +x sparkNodeSetup.sh
 ./sparkNodeSetup.sh slave
 . ~/.bashrc
