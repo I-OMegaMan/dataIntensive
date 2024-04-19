4ad78769def14b51c34ce37bfb512f27ccee2d8e
mmyers <nt3058@gmail.com>
2024-04-19 08:33:10 -0400

    removed un-needed data from mapper print

--- project/python/mapper.py
+++ project/python/mapper.py
@@ -5,21 +5,21 @@
 import sys
 import hashlib
 import secretMessage as sm
+import io
+input_stream = io.TextIOWrapper(sys.stdin.buffer, encoding='ISO-8859-15')
 #Ravioli Ravioli!
 theSecretMessage = bytes.fromhex("d892c6718a7af2cd14a9f6056795279b")
 
 
 # input comes from STDIN (standard input)
-
-for line in sys.stdin:
+for line in input_stream:
     # remove leading and trailing whitespace
     line = line.strip()
     h = hashlib.new('sha256')
     h.update(line.encode('utf-8'))
-
     # write the results to STDOUT (standard output);
     # what we output here will be the input for the
     # Reduce step, i.e. the input for reducer.py
     #print("%s\t%s" % (bytes(h.hexdigest()).hex(), 1))
     decryptRes = sm.quickDecrypt(bytes.fromhex(h.hexdigest()),theSecretMessage)
-    print("%s\t%s\t%s" % (line, decryptRes.hex(),1))
+    print("%s\t%s" % (line, decryptRes.hex()))
